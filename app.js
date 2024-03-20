5b0b3935a62fa8d35e0c2800f4e7cfb91069c7ff
octophetus <octophetus@gmail.com>
2024-03-20 13:01:34 -0400

    finished problem set

--- app.js
+++ app.js
@@ -21,8 +21,6 @@ const avgNums = (nums) => {
   return avg;
 };
 
-//-----------------------VARIABLES AND FUNCTION CALLS--------------------------//
-
 // Take an array of strings and return the longest string.
 const longestStrings = (strings) => {
   let longestString = "";
@@ -35,5 +33,34 @@ const longestStrings = (strings) => {
 };
 
 // Take an array of strings, and a number and return an array of the strings that are longer than the given number.
-// For example, stringsLongerThan(['say', 'hello', 'in', 'the', 'morning'], 3); would return ["hello", "morning"].
+const findLongerStrings = (strings, num) => {
+  let longerStrings = [];
+  strings.forEach((string) => {
+    if (string.length > num) {
+      longerStrings.push(string);
+    }
+  });
+  return longerStrings;
+};
+
+//-----------------------VARIABLES AND FUNCTION CALLS--------------------------//
+
 // Take a number, n, and print every number between 1 and n without using loops. Use recursion.
+
+// const printNums = (num) => {
+//   for (let i = 0; i < nums; i++) {
+//     console.log(i);
+//   }
+// };
+
+// Also works, but less good of a solution:
+
+// let count = 1;
+// const printNumsWhile = (num) => {
+//   while (count < num) {
+//     console.log(count);
+//     count++;
+//   }
+// };
+
+// OOOPS just saw it said without using loops, lol ok. Umm... I am goign to need to think on that one, sorry.
